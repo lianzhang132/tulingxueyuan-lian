b27cafcb4e66950f78162879323341e27f3e9ee2
lianzhang132 <2327431669@qq.com>
2018-11-07 16:49:49 +0800

    Initial commit

---
+++ oop/01.py
@@ -0,0 +1,24 @@
+class Student():
+    pass
+
+mingyue = Student()
+
+
+class PythonStudent():
+
+    name = None
+    age = 18
+    course = "Python"
+
+
+    def doHomework(self):
+        print("I   ZAI zuozuoye")
+
+
+        return None
+
+yueyue = PythonStudent()
+print(yueyue.name)
+print(yueyue.age)
+yueyue.doHomework()
+
